a889e491b404bde7c9fa94cfce1b334e48e41655
Ge0rg3e <Ge0rg3e@users.noreply.github.com>
2024-01-08 12:15:35 +0000

    publish: output for ec8ad0fe11263682f39d5479c82b5a93f3d184e9

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -2,6 +2,10 @@ export declare type LanguagesCallback = (args?: any) => string;
 export interface Languages {
     EN: LanguagesCallback | string;
     RO?: LanguagesCallback | string;
+    DE?: LanguagesCallback | string;
+    ES?: LanguagesCallback | string;
+    IT?: LanguagesCallback | string;
+    RU?: LanguagesCallback | string;
 }
 export interface LanguagePack {
     [key: string]: Languages;
