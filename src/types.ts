008633570cb7f2e80574539b8b20bbb84e8a5490
Ge0rg3e <ge0rg3ee@gmail.com>
2024-01-08 14:00:33 +0200

    Added German language

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ export type LanguagesCallback = (args?: any) => string;
 export interface Languages {
 	EN: LanguagesCallback | string;
 	RO?: LanguagesCallback | string;
+	DE?: LanguagesCallback | string;
 }
 
 export interface LanguagePack {
