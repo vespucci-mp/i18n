d5b857e274bcd01b411c169a28ee9c93b1b08801
Ge0rg3e <Ge0rg3e@users.noreply.github.com>
2024-01-08 14:54:37 +0000

    publish: output for b13f630522ae71c2b8f473dc69018420c0878f47

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import { LanguagePack, BaseLanguagePack } from './types';
 export declare const languagePacks: BaseLanguagePack;
 export declare const createLanguagePack: (systemId: string, messages: LanguagePack, allowOverwrites?: boolean | undefined) => Promise<void>;
-export declare const getLanguagePack: (systemId: string, specificLanguage?: "RO" | "EN" | undefined) => {
+export declare const getLanguagePack: (systemId: string, specificLanguage?: "RO" | "EN" | "DE" | "ES" | "IT" | "RU" | undefined) => {
     get: (messageId: string, args?: object | undefined) => string;
 };
 export * from './types';
